7103aa949188beef74206c74128575a37666e45f
priyankabhavanam <100194106+priyankabhavanam@users.noreply.github.com>
2022-03-31 23:54:06 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '1c9bedcdeb3825d87c4dbabcec3c767a61767f96',
+	    'sonar.login': '6819d1c3fdcf307311a8c01df528dfd3c56edefd',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
